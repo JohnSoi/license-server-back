c0e1c4982d37cc80ddd4791833406c2af606726b
JohnSoi <un.perso@yandex.ru>
2022-07-14 22:25:29 +0500

    Глобальные правки
    1. Переделал архитектуру входной точки
    2. Добавил поиск и фильтр в Лицензии
    3.

--- classes/EndpointFactory.py
+++ classes/EndpointFactory.py
@@ -38,7 +38,12 @@ class EndpointFactory:
             raise RuntimeError('Метод не найден в списке доступных')
 
         self._method = self._class.methods_map[params.get('method')]
-        self._data = params.get('data')
+        data = params.get('data') or {}
+        self._data = data.get('params')
+        self._filter = data.get('filter')
+        self._navigation = data.get('navigation')
+        self._sorting = data.get('sorting')
 
     def process(self):
-        return self._method(self._data)
+        return self._method(data=self._data, filter=self._filter,
+                            navigation=self._navigation, sorting=self._sorting)
--- config/common.py
+++ config/common.py
@@ -7,6 +7,7 @@ SECRET_KEY = 'gq1KJC]=YdL,D]&hNK|c[|ho&O:Msz#T^rlDBc6sk$ayUAf#a|LH$boOD[%Usnr'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{LOGIN}:{PASSWORD}@localhost/{NAME}'
+SQLALCHEMY_ECHO = True
 
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 UPLOAD_FOLDER = 'uploads'
