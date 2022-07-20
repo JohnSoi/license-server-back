487a5c3d8f989cb0e092fd12673287c2b54beffe
TelegaStrasti <baigildin.idel@yandex.ru>
2022-07-20 16:23:26 +0500

    фильтрация клиентов

--- classes/Client.py
+++ classes/Client.py
@@ -1,3 +1,4 @@
+
 from models.Client import Client as ClientModel
 from classes.BaseClass import BaseClass
 
@@ -10,16 +11,13 @@ class Client(BaseClass):
     @classmethod
     def _prepare_query_filter(cls, query, filter_params):
         if filter_params:
+            if filter_params.get('searchString'):
+                query = query.where(cls.get_model().name.like(f'%{filter_params.get("searchString")}%'))
             if filter_params.get('dateStart'):
                 query = query.where(cls.get_model().create_at > filter_params.get('dateStart'))
             if filter_params.get('dateEnd'):
                 query = query.where(cls.get_model().create_at < filter_params.get('dateEnd'))
-            if filter_params.get('isActive') == False:
-                query = query.where(cls.get_model().is_active == 0)
-            if filter_params.get('isActive' == True):
-                query = query.where(cls.get_model().is_active == 1)
-            if filter_params.get('isPaid') == False:
-                query = query.where(cls.get_model().license_uuid != None)
-            if filter_params.get('isPaid') == True:
-                query = query.where(cls.get_model().license_uuid == 0)
-        return query
+            if filter_params.get('isActive') is not None:
+                query = query.where(cls.get_model().is_active == filter_params.get('isActive'))
+            return query
+
